15d2768815dda03ce39b5163c66912708fc3765f
Christopher Galpin <52485+CodeOptimist@users.noreply.github.com>
2019-10-28 04:07:19 -0600

    global yaml as cmd-line arg; prefer mod then global yaml variables

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ from collections import defaultdict
 from lxml import etree
 from lxml.builder import E
 
-_global_path = os.path.join(os.getcwd(), r'global.yaml')
 global_yaml = {}
-if os.path.exists(_global_path):
-    with open(_global_path, encoding='utf-8') as f:
-        global_yaml = yaml.safe_load(f)
 mod_yaml = {}
 
 
@@ -21,7 +17,17 @@ def main():
         if os.path.isdir(yaml_path):
             yaml_path = os.path.join(yaml_path, r'public.yaml')
         with open(yaml_path, encoding='utf-8') as f:
-            mod_yaml = yaml.safe_load(f)
+            _data = yaml.safe_load(f)
+
+        mod_yaml = {}
+        for k, v in _data.items():
+            if k.startswith('global_'):
+                global_yaml[k] = v
+            else:
+                mod_yaml[k] = v
+
+        if 'features' not in mod_yaml:
+            continue
 
         markup = get_steam_markup()
         about = get_about()
@@ -29,15 +35,15 @@ def main():
         settings = get_settings()
 
         dir_name = os.path.dirname(yaml_path)
-        write_xml(dir_name, [mod_yaml.get('about_path'), r'About\About.xml'], about)
+        write_xml(dir_name, [prefer_local('about_path', ""), r'About\About.xml'], about)
         if updates.getchildren():
             write_xml(dir_name, [
-                mod_yaml.get('updates_path'),
+                prefer_local('updates_path', ""),
                 r'Defs\UpdateFeatures.xml',
                 r'Defs\UpdateFeatureDefs\UpdateFeatures.xml',
             ], updates)
         if settings.getchildren():
-            write_xml(dir_name, [mod_yaml.get('settings_path'), r'Languages\English\Keyed\Settings.xml'], settings)
+            write_xml(dir_name, [prefer_local('settings_path', ""), r'Languages\English\Keyed\Settings.xml'], settings)
 
         # ahk = Script()
         # ahk.set('clipboard', markup)
@@ -46,43 +52,63 @@ def main():
         print('-' * 100)
 
 
-def get_with_features(is_xml, features=None, feature_filter=lambda x: x, format_=None):
-    format_ = global_yaml.get(format_, "{features}")
+def prefer_local(key, default=None):
+    if key is None:
+        return default
+    is_required = default is None
+    if is_required:
+        return mod_yaml.get(key, global_yaml['global_' + key])
+    return mod_yaml.get(key, global_yaml.get('global_' + key, default))
+
+
+def get_with_features(format_, is_xml, features=None, feature_filter=lambda x: x):
     if features is None:
         features = mod_yaml['features']
-    xml_features = global_yaml.get('xml_feature_format', "<color=teal><b>{title}</b></color>\n{desc}")
-    steam_features = global_yaml.get('steam_feature_format', "[b]{title}[/b]\n[i]{steam}[/i]\n{desc}")
-    feature_format = xml_features if is_xml else steam_features
+    if format_ is None:
+        format_ = "{features}"
+
+    steam_feature_format = prefer_local('steam_feature_format',
+                                        "[b]{feature_title}[/b]\n[i]{feature_steam}[/i]\n{feature_desc}")
+    xml_feature_format = prefer_local('xml_feature_format',
+                                      "<color=white><b>{feature_title}</b></color>\n{feature_desc}")
+    feature_format = xml_feature_format if is_xml else steam_feature_format
 
     lines = []
-    for _feature in filter(feature_filter, features):
-        feature = defaultdict(str, _feature)
-        format_exist = "\n".join(l for l in feature_format.split("\n") if re.sub(r'</?.*?>' if is_xml else r'\[/?.*?]', '', l.format_map(feature)))
-        lines.append(format_exist.format_map(feature) + "\n")
-    feature_text = "\n".join(lines).strip()
+    for feature in filter(feature_filter, features):
+        feature_lines = []
+        feature_scope = defaultdict(str, {**{'feature_' + k: v for k, v in feature.items()}, **mod_yaml, **global_yaml})
+        for line in feature_format.split("\n"):
+            text = line.format_map(feature_scope).format_map(feature_scope)
+            inner_text = re.sub(r'</?.*?>' if is_xml else r'\[/?.*?]', '', text)
+            if inner_text:
+                feature_lines.append(text)
+        lines.append("\n".join(feature_lines))
+    features_text = "\n\n".join(lines)
     if is_xml:
-        feature_text = markup_to_xml(feature_text) + "\n"  # without ending \n, text can be cut off
+        features_text = markup_to_xml(features_text)
 
-    all_data = defaultdict(str, {**mod_yaml, **global_yaml})
-    all_data['features'] = feature_text  # for a friendly key name within formats
-    text = format_.format_map(all_data).format_map(all_data)
+    scope = defaultdict(str, {**mod_yaml, **global_yaml})
+    scope['features'] = features_text  # for a friendly key name within formats
+    text = format_.format_map(scope).format_map(scope) + "\n"  # without ending \n, xml text can be cut off
     return text
 
 
 def get_steam_markup():
-    result = get_with_features(is_xml=False, feature_filter=lambda x: x.get('title'), format_='steam_format')
+    result = get_with_features(format_=prefer_local('steam_format', "{features}"), is_xml=False,
+                               feature_filter=lambda x: x.get('title'))
     return result
 
 
 def get_about():
-    description = get_with_features(is_xml=True, feature_filter=lambda x: x.get('title'), format_='about_format')
+    description = get_with_features(format_=prefer_local('about_format', markup_to_xml(prefer_local(
+        'steam_format', "{features}"))), is_xml=True, feature_filter=lambda x: x.get('title'))
     supported_versions = E.supportedVersions()
-    for v in mod_yaml['supported_versions']:
+    for v in prefer_local('supported_versions'):
         supported_versions.append(E.li(str(v)))
     result = E.ModMetaData(
         E.name(mod_yaml['name']),
-        E.author(mod_yaml.get('author', global_yaml['author'])),
-        E.url(mod_yaml.get('url', mod_yaml.get('repo'))),
+        E.author(prefer_local('author')),
+        E.url(prefer_local('url', "")),
         supported_versions,
         E.description(etree.CDATA(description)),
     )
@@ -95,7 +121,7 @@ def get_updates():
           {'Abstract': "true", 'Name': "UpdateFeatureBase"},
           E.modNameReadable(mod_yaml['name']),
           E.modIdentifier(mod_yaml['identifier']),
-          E.linkUrl(mod_yaml.get('url', mod_yaml.get('repo'))),
+          E.linkUrl(prefer_local('url', "")),
           )
     )
 
@@ -104,12 +130,14 @@ def get_updates():
         _version_features = {v: [f for f in mod_yaml['features'] if f.get('at') == v] for v in versions}
         return _version_features
 
-    reverse = mod_yaml.get('descending_updates', global_yaml.get('descending_updates', True))
+    reverse = prefer_local('descending_updates', True)
     for version, features in sorted(version_features().items(), reverse=reverse):
-        content = get_with_features(is_xml=True, features=features)
+        update_scope = defaultdict(str, {'update_version': version.replace(r'.', r'_'), **mod_yaml, **global_yaml})
+        content = get_with_features(format_="{features}", is_xml=True, features=features)
         element = E("HugsLib.UpdateFeatureDef",
                     {'ParentName': "UpdateFeatureBase"},
-                    E.defName(mod_yaml['identifier'] + '_' + version.replace(r'.', r'_')),
+                    E.defName(
+                        prefer_local('update_defname_format', "{identifier}_{update_version}").format_map(update_scope).format_map(update_scope)),
                     E.assemblyVersion(version),
                     E.content(etree.CDATA(content)),
                     )
@@ -127,15 +155,17 @@ def get_settings():
         for setting in feature.get('settings', []):
             name = setting['name']
             gathered[name]['title'] += (val(setting.get('title'), feature['title']))
-            gathered[name]['desc'] += (val(setting.get('desc'), feature.get('desc')))
+            gathered[name]['desc'] += (val(setting.get('desc'), feature.get('desc', "")))
 
     result = E.LanguageData()
-    for setting in gathered:
-        title = E("{}_{}Setting_title".format(mod_yaml['prefix'], setting),
-                  etree.CDATA(markup_to_xml(re.sub(r'\.$', r'', gathered[setting]['title']))))
+    for name, setting in gathered.items():
+        setting_scope = defaultdict(str, {'setting_name': name, **mod_yaml, **global_yaml})
+        title = E(prefer_local('setting_title_key_format', "{identifier}_{setting_name}_SettingTitle").format_map(setting_scope).format_map(setting_scope),
+                  etree.CDATA(markup_to_xml(re.sub(r'\.$', r'', setting['title']))))
         result.append(title)
-        desc = E("{}_{}Setting_description".format(mod_yaml['prefix'], setting),
-                 etree.CDATA(markup_to_xml(gathered[setting]['desc'])))
+        desc = E(
+            prefer_local('setting_desc_key_format', "{identifier}_{setting_name}_SettingDesc").format_map(setting_scope).format_map(setting_scope),
+            etree.CDATA(markup_to_xml(setting['desc'])))
         result.append(desc)
 
     return result
@@ -153,9 +183,9 @@ def write_xml(base_path, rel_paths, root):
 
 def markup_to_xml(text):
     result = text
-    url_format = global_yaml.get('xml_url_format', r'<color=grey><b>{text}</b></color>').format(url=r'\1', text=r'\2')
+    url_format = prefer_local('xml_url_format', r'<color=grey><b>{text}</b></color>').format(url=r'\1', text=r'\2')
     result = re.sub(r'\[url=(.*?)](.*?)\[/url]', url_format, result, flags=re.DOTALL)
-    u_format = global_yaml.get('xml_u_format', r'<color=grey>{text}</color>').format(text=r'\1')
+    u_format = prefer_local('xml_u_format', r'<color=grey>{text}</color>').format(text=r'\1')
     result = re.sub(r'\[u](.*?)\[/u]', u_format, result, flags=re.DOTALL)
 
     count = -1
