8a30212c547c5253ac3c827cb1d84c18e5bcb3a8
Christopher Galpin <52485+CodeOptimist@users.noreply.github.com>
2021-09-07 06:17:20 -0600

    rearrange

--- main.py
+++ main.py
@@ -38,9 +38,106 @@ def main() -> None:
     g.setdefault('local_dir', yaml_path.parent)
     g.setdefault('working_dir', os.getcwd())
 
+    def get_steam() -> str:
+        formatted_features = ''.join(f.format(g.steam_feature_format, l=feature) for feature in g.features).strip()
+        result = f.format(g.steam_format, l={'features': formatted_features})
+        return result
+
     steam = get_steam()
+
+    def get_about() -> _Element:
+        root = E.ModMetaData()
+        formatted_features = ''.join(f.format(g.about_feature_format, l=feature) for feature in g.features).strip()
+        populate_xml(root, g.ModMetaData, locals={'features': formatted_features})
+        return root
+
     about = get_about()
+
+    def populate_xml(xml_element: _Element, value: Any, *, locals: Optional[dict] = None) -> None:
+        if isinstance(dict_ := value, dict):
+            for k, v in dict_.items():
+                if v is None:  # optional
+                    continue
+                k = f.format(k, l=locals)
+                xml_child = E(k)
+                populate_xml(xml_child, v, locals=locals)
+
+                if isinstance(v, Steam2Xml):
+                    def steam_to_xml(text: str) -> str:
+                        result = text
+                        url_format = f.format(g.xml_url_format, l={'url': r'\1', 'text': r'\2'})
+                        result = re.sub(r'\[url=(.*?)](.*?)\[/url]', url_format, result, flags=re.DOTALL | re.IGNORECASE)
+                        u_format = f.format(g.xml_u_format, l={'text': r'\1'})
+                        result = re.sub(r'\[u](.*?)\[/u]', u_format, result, flags=re.DOTALL | re.IGNORECASE)
+
+                        result = re.sub(r'\n\[img](.*?)\[/img]\n', '\n', result, flags=re.DOTALL | re.IGNORECASE)
+                        result = re.sub(r'\[img](.*?)\[/img]', '', result, flags=re.DOTALL | re.IGNORECASE)
+
+                        count = -1
+                        while count != 0:
+                            result, count = re.subn(r'\[(\w+)(=\w+)?](.*?)\[/\1]', r'<\1\2>\3</\1>', result, flags=re.DOTALL)
+                        return result
+
+                    # noinspection PyTypeChecker
+                    xml_child.text = etree.CDATA(steam_to_xml(xml_child.text))
+
+                is_attribute = k.startswith('_') and len(xml_child) == 0
+                if is_attribute:
+                    xml_element.set(k[1:], f.format(str(v), l=locals))
+                else:
+                    xml_element.append(xml_child)
+        elif isinstance(list_ := value, list):
+            for e in list_:
+                xml_child = E.li()
+                populate_xml(xml_child, e, locals=locals)
+                xml_element.append(xml_child)
+        else:
+            assert value is not None, f'{xml_element}'
+            xml_element.text = f.format(str(value), l=locals)
+
+    def get_updates() -> _Element:
+        update_parent = E('HugsLib.UpdateFeatureDef')
+        populate_xml(update_parent, g.UpdateFeatureDefBase)
+        root = E.Defs(update_parent)
+
+        def version_updates() -> dict:
+            versions = set(at for u in g.features if (at := u.get('at'))) | set(u['at'] for u in g.updates)
+            result = {}
+            for version in versions:
+                feature_updates = [u for u in g.features if u.get('at') == version]
+                standalone_updates = [u for u in g.updates if u['at'] == version]
+                result[version] = feature_updates + standalone_updates
+            return result
+
+        # ascending sort is mandatory with HugsLib on 1.1 or LastSeenNews.xml will update wrong
+        for version, updates in sorted(version_updates().items()):
+            update = E('HugsLib.UpdateFeatureDef')
+            # end the xml element with only one newline
+            formatted_updates = ''.join(f.format(g.update_feature_format, l=update) for update in updates).strip()
+            populate_xml(update, g.UpdateFeatureDef, locals={'version': version, 'updates': formatted_updates})
+            root.append(update)
+        return root
+
     updates = get_updates()
+
+    def get_settings() -> _Element:
+        # name key must be on the setting itself
+        settings = [{**feature, 'name': None, **setting} for feature in g.features for setting in feature.get('settings', [])]
+        settings += g.settings
+
+        root = E.LanguageData()
+        for setting in settings:
+            setting: dict
+            assert setting['name'], setting
+            assert 'title' in setting, setting
+
+            setting['title'] = re.sub(r'\b\.$', '', setting['title'])
+            xml_container = E('temp')
+            populate_xml(xml_container, g.SettingLanguageData, locals=setting)
+            for setting_element in xml_container:
+                root.append(setting_element)
+        return root
+
     settings = get_settings()
 
     if g.preview_from_path:
@@ -71,105 +168,6 @@ def main() -> None:
     print('-' * 100)
     print(steam)
     print('-' * 100)
-
-
-def get_steam() -> str:
-    formatted_features = ''.join(f.format(g.steam_feature_format, l=feature) for feature in g.features).strip()
-    result = f.format(g.steam_format, l={'features': formatted_features})
-    return result
-
-
-def populate_xml(xml_element: _Element, value: Any, *, locals: Optional[dict] = None) -> None:
-    def steam_to_xml(text: str) -> str:
-        result = text
-        url_format = f.format(g.xml_url_format, l={'url': r'\1', 'text': r'\2'})
-        result = re.sub(r'\[url=(.*?)](.*?)\[/url]', url_format, result, flags=re.DOTALL | re.IGNORECASE)
-        u_format = f.format(g.xml_u_format, l={'text': r'\1'})
-        result = re.sub(r'\[u](.*?)\[/u]', u_format, result, flags=re.DOTALL | re.IGNORECASE)
-
-        result = re.sub(r'\n\[img](.*?)\[/img]\n', '\n', result, flags=re.DOTALL | re.IGNORECASE)
-        result = re.sub(r'\[img](.*?)\[/img]', '', result, flags=re.DOTALL | re.IGNORECASE)
-
-        count = -1
-        while count != 0:
-            result, count = re.subn(r'\[(\w+)(=\w+)?](.*?)\[/\1]', r'<\1\2>\3</\1>', result, flags=re.DOTALL)
-        return result
-
-    if isinstance(dict_ := value, dict):
-        for k, v in dict_.items():
-            if v is None:  # optional
-                continue
-            k = f.format(k, l=locals)
-            xml_child = E(k)
-            populate_xml(xml_child, v, locals=locals)
-
-            if isinstance(v, Steam2Xml):
-                # noinspection PyTypeChecker
-                xml_child.text = etree.CDATA(steam_to_xml(xml_child.text))
-
-            is_attribute = k.startswith('_') and len(xml_child) == 0
-            if is_attribute:
-                xml_element.set(k[1:], f.format(str(v), l=locals))
-            else:
-                xml_element.append(xml_child)
-    elif isinstance(list_ := value, list):
-        for e in list_:
-            xml_child = E.li()
-            populate_xml(xml_child, e, locals=locals)
-            xml_element.append(xml_child)
-    else:
-        assert value is not None, f'{xml_element}'
-        xml_element.text = f.format(str(value), l=locals)
-
-
-def get_about() -> _Element:
-    root = E.ModMetaData()
-    formatted_features = ''.join(f.format(g.about_feature_format, l=feature) for feature in g.features).strip()
-    populate_xml(root, g.ModMetaData, locals={'features': formatted_features})
-    return root
-
-
-def get_updates() -> _Element:
-    update_parent = E('HugsLib.UpdateFeatureDef')
-    populate_xml(update_parent, g.UpdateFeatureDefBase)
-    root = E.Defs(update_parent)
-
-    def version_updates() -> dict:
-        versions = set(at for u in g.features if (at := u.get('at'))) | set(u['at'] for u in g.updates)
-        result = {}
-        for version in versions:
-            feature_updates = [u for u in g.features if u.get('at') == version]
-            standalone_updates = [u for u in g.updates if u['at'] == version]
-            result[version] = feature_updates + standalone_updates
-        return result
-
-    # ascending sort is mandatory with HugsLib on 1.1 or LastSeenNews.xml will update wrong
-    for version, updates in sorted(version_updates().items()):
-        update = E('HugsLib.UpdateFeatureDef')
-        # end the xml element with only one newline
-        formatted_updates = ''.join(f.format(g.update_feature_format, l=update) for update in updates).strip()
-        populate_xml(update, g.UpdateFeatureDef, locals={'version': version, 'updates': formatted_updates})
-        root.append(update)
-    return root
-
-
-def get_settings() -> _Element:
-    # name key must be on the setting itself
-    settings = [{**feature, 'name': None, **setting} for feature in g.features for setting in feature.get('settings', [])]
-    settings += g.settings
-
-    root = E.LanguageData()
-    for setting in settings:
-        setting: dict
-        assert setting['name'], setting
-        assert 'title' in setting, setting
-
-        setting['title'] = re.sub(r'\b\.$', '', setting['title'])
-        xml_container = E('temp')
-        populate_xml(xml_container, g.SettingLanguageData, locals=setting)
-        for setting_element in xml_container:
-            root.append(setting_element)
-    return root
 
 
 if __name__ == '__main__':
